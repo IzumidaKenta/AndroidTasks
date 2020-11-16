b1e091f9f3d4e7503911c0cf978d8797a4c95740
KentaIzumida-tg <kenta.izumida@topgate.co.jp>
2020-11-16 14:45:18 +0900

    feat:返信がランダムになるようにした

--- task5/app/src/main/java/com/example/task5/MainActivity.kt
+++ task5/app/src/main/java/com/example/task5/MainActivity.kt
@@ -29,18 +29,22 @@ class MainActivity : AppCompatActivity() {
         sendButton.setOnClickListener {
             if (messageEditText != null) {
                 val newMessage = Message(1, messageEditText.text.toString(), getNowTime())
-                val replyMessage = Message(2, "Hello", getNowTime())
+                val replyMessage = Message(2, randomReply(), getNowTime())
                 messageEditText.editableText.clear()
                 mMessageList.add(newMessage)
                 mMessageList.add(replyMessage)
                 //リストアイテムの総数-1（0番目から始まって最後のアイテム）にスクロールさせる
-                //リストアイテムの総数-1（0番目から始まって最後のアイテム）にスクロールさせる
-
                 Handler().postDelayed(Runnable {
                     listView.smoothScrollToPosition(listView.count - 1)
                 }, 100)
             }
         }
+    }
+
+    fun randomReply(): String {
+        val replyList = arrayListOf<String>("Hello", "GOODBYE", "THANK YOU", "GOOD", "YEAH")
+        val index = Random().nextInt(replyList.size)
+        return replyList[index]
     }
 
     fun getNowTime(): String {
