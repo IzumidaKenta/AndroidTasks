26c6749a2860060f647ebb3bac1534ddf12d012a
KentaIzumida-tg <kenta.izumida@topgate.co.jp>
2020-11-18 14:59:07 +0900

    feat:firestoreにメッセージを送信できるようにした

--- task5/app/src/main/java/com/example/task5/MainActivity.kt
+++ task5/app/src/main/java/com/example/task5/MainActivity.kt
@@ -3,16 +3,18 @@ package com.example.task5
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.util.Log
 import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.activity_main.*
 import java.text.SimpleDateFormat
 import java.util.*
 
-
 class MainActivity : AppCompatActivity() {
 
     lateinit var mCustomAdapter: CustomAdapter
+    val db = FirebaseFirestore.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,8 +34,8 @@ class MainActivity : AppCompatActivity() {
                 val newMessage = Message(1, messageEditText.text.toString(), getNowTime())
                 val replyMessage = Message(2, randomReply(), getNowTime())
                 messageEditText.editableText.clear()
-                mMessageList.add(newMessage)
-                mMessageList.add(replyMessage)
+                add(newMessage)
+                add(replyMessage)
                 //リストアイテムの総数-1（0番目から始まって最後のアイテム）にスクロールさせる
                 Handler(Looper.getMainLooper()).postDelayed(Runnable {
                     listView.smoothScrollToPosition(listView.count - 1)
@@ -52,6 +54,25 @@ class MainActivity : AppCompatActivity() {
         val date = Date()
         val format = SimpleDateFormat("HH:mm", Locale.getDefault())
         return format.format(date)
+    }
+
+    fun add(message: Message) {
+        db.collection("talks")
+            .add(message.toMap())
+            .addOnSuccessListener { documentReference ->
+                Log.d("add", "DocumentSnapshot added : ${documentReference.id}")
+            }
+            .addOnFailureListener { e ->
+                Log.w("add", "Error adding document", e)
+            }
+    }
+
+    fun Message.toMap(): Map<String, *> {
+        return hashMapOf(
+            "type" to this.type,
+            "message" to this.message,
+            "sendTime" to this.sendTime
+        )
     }
 }
 
