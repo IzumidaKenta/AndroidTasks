2a42ad35de8733dcb05b5136390c3220aa006394
KentaIzumida-tg <kenta.izumida@topgate.co.jp>
2020-11-19 18:07:02 +0900

    feat:GridViewを配置した

---
+++ task6/app/src/main/java/com/example/task6/GridAdapter.kt
@@ -0,0 +1,30 @@
+package com.example.task6
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.TextView
+
+class GridAdapter(context: Context, var mImageList: List<String>) :
+    ArrayAdapter<String>(context, 0, mImageList) {
+
+    private val layoutInflater =
+        context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+
+        var view = convertView
+        val imageItem = mImageList[position]
+
+        if (convertView == null) {
+            view = layoutInflater.inflate(R.layout.grid_items, parent, false)
+        }
+
+        val message = view?.findViewById<TextView>(R.id.url)
+        message?.text = imageItem
+
+        return view!!
+    }
+}
--- task6/app/src/main/java/com/example/task6/MainActivity.kt
+++ task6/app/src/main/java/com/example/task6/MainActivity.kt
@@ -2,11 +2,26 @@ package com.example.task6
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val imageList = arrayListOf<String>()
+        imageList.add("https:1")
+        imageList.add("https:2")
+        imageList.add("https:3")
+        imageList.add("https:4")
+        imageList.add("https:5")
+        imageList.add("https:6")
+        imageList.add("https:7")
+        imageList.add("https:8")
+        imageList.add("https:9")
+
+        val gridAdapter = GridAdapter(this, imageList)
+        gridView.adapter = gridAdapter
     }
 }
