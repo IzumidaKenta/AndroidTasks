57fd2010ebc4cd4b1b77f8e17a5572b5e2a08292
KentaIzumida-tg <kenta.izumida@topgate.co.jp>
2020-11-19 19:09:22 +0900

    feat:Firestoreを依存関係に追加した

--- task6/app/src/main/java/com/example/task6/MainActivity.kt
+++ task6/app/src/main/java/com/example/task6/MainActivity.kt
@@ -2,9 +2,12 @@ package com.example.task6
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    val db = FirebaseFirestore.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
