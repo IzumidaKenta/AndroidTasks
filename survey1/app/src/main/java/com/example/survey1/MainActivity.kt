1096a9a7e0e7d5ca906d5c8e79489a241437cc9c
KentaIzumida-tg <kenta.izumida@topgate.co.jp>
2020-11-25 18:30:26 +0900

    feat:ひとつ目のEditTextに6文字入力したら自動でフォーカスが移動するようにした

--- survey1/app/src/main/java/com/example/survey1/MainActivity.kt
+++ survey1/app/src/main/java/com/example/survey1/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.survey1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import androidx.core.widget.doOnTextChanged
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -10,9 +13,20 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        if(editText1.length() == 6) {
-            editText1.clearFocus()
-            editText2.requestFocus()
-        }
+        editText1.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(p0: Editable?) {
+            }
+
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                if (editText1.length() == 6) {
+                    editText1.clearFocus()
+                    editText2.requestFocus()
+                }
+            }
+        })
     }
+
 }
