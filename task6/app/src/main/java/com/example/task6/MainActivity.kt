5704cd9b0ffa41763c4cb89f4defe86acaa2049a
KentaIzumida-tg <kenta.izumida@topgate.co.jp>
2020-12-02 16:49:52 +0900

    feat:GridViewを生成する処理を関数に分けた

--- task6/app/src/main/java/com/example/task6/MainActivity.kt
+++ task6/app/src/main/java/com/example/task6/MainActivity.kt
@@ -3,44 +3,42 @@ package com.example.task6
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import com.google.android.gms.tasks.OnCompleteListener
-import com.google.android.gms.tasks.Task
 import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.firestore.QuerySnapshot
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    val db = FirebaseFirestore.getInstance()
-    val context = this
+    private val db = FirebaseFirestore.getInstance()
+    private val context = this
+    private val mImageList = arrayListOf<ImagePass>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val mImageList = arrayListOf<ImagePass>()
+        makeGridView()
 
-        //データベースのメッセージを読み込んでListViewを生成
+    }
+
+    private fun makeGridView() {
+        //データベースのimagePassを読み込んでGridViewを生成
         db.collection("imagePass").orderBy("id").get()
-            .addOnCompleteListener(object : OnCompleteListener<QuerySnapshot> {
-                override fun onComplete(task: Task<QuerySnapshot>) {
-                    if (task.isSuccessful) {
-                        for (document in task.result!!) {
-                            val imagePass: ImagePass =
-                                document.toObject<ImagePass>(ImagePass::class.java)
-                            mImageList.add(imagePass)
-                        }
-                        val gridAdapter = GridAdapter(context, mImageList)
-                        gridView.adapter = gridAdapter
-                    } else {
-                        Log.d(
-                            "MissionActivity",
-                            "Error getting documents: ",
-                            task.exception
-                        )
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    for (document in task.result!!) {
+                        val imagePass: ImagePass =
+                            document.toObject<ImagePass>(ImagePass::class.java)
+                        mImageList.add(imagePass)
                     }
+                    val gridAdapter = GridAdapter(context, mImageList)
+                    gridView.adapter = gridAdapter
+                } else {
+                    Log.d(
+                        "MissionActivity",
+                        "Error getting documents: ",
+                        task.exception
+                    )
                 }
-            })
-
+            }
     }
 }
