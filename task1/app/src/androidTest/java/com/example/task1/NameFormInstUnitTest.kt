4f73142140750cf888bde3bc083455ff039b87f7
KentaIzumida-tg <kenta.izumida@topgate.co.jp>
2020-12-10 14:59:28 +0900

    fix:ファイル名をクラス名に合わせた feat:Instテストでエラーが起こらないようにした feat:Instテストでエラーが起こらないようにした

--- task1/app/src/androidTest/java/com/example/task1/NameFormInstUnitTest.kt
+++ task1/app/src/androidTest/java/com/example/task1/NameFormInstUnitTest.kt
@@ -1,22 +1,23 @@
 package com.example.task1
-import android.os.Build
+
 import android.widget.EditText
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.rule.ActivityTestRule
 import org.junit.Assert.assertTrue
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.robolectric.Robolectric
-import org.robolectric.RobolectricTestRunner
-import org.robolectric.annotation.Config
 
-@RunWith(RobolectricTestRunner::class)
-@Config(sdk = [Build.VERSION_CODES.O_MR1])
+@RunWith(AndroidJUnit4::class)
 class NameFormInstUnitTest {
+    @get:Rule
+    val activityRule = ActivityTestRule(MainActivity::class.java)
+
     @Test
     fun nameFormText_longerThan10characters_ExceptionThrown() {
-        val activity:MainActivity = Robolectric.buildActivity(MainActivity::class.java).setup().get()
+        val activity: MainActivity = activityRule.activity
         val editText: EditText = activity.findViewById<EditText>(R.id.name_form)
         val textLength = editText.text.length
-        print("TEST " + textLength)
         assertTrue(textLength < 10)
     }
 }
