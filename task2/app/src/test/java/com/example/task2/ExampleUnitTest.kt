8b31dd4d55bd48c640d921bfd4275177aa712355
KentaIzumida-tg <kenta.izumida@topgate.co.jp>
2020-12-14 12:00:58 +0900

    feat:テストからMainActivityのメソッドを呼び出すようにした

--- task2/app/src/test/java/com/example/task2/ExampleUnitTest.kt
+++ task2/app/src/test/java/com/example/task2/ExampleUnitTest.kt
@@ -22,29 +22,29 @@ import org.robolectric.annotation.Config
 @RunWith(RobolectricTestRunner::class)
 @Config(sdk = [Build.VERSION_CODES.O_MR1])
 class ExampleUnitTest {
-    val activity:MainActivity = Robolectric.buildActivity(MainActivity::class.java).setup().get()
+    val activity: MainActivity = Robolectric.buildActivity(MainActivity::class.java).setup().get()
 
     @Test
     fun nameFormText_longerThan10characters_ExceptionThrown() {
         val nameFormEt: EditText = activity.findViewById<EditText>(R.id.name_form)
         nameFormEt.setText("あいうえお")
-        val textLength = nameFormEt.text.length
-        assertTrue(textLength < 10)
+        assertTrue(activity.textLongerThanLimit(nameFormEt, 10))
     }
 
     @Test
     fun mailAddressFormText_longerThan20characters_ExceptionThrown() {
         val mailAddressFormEt: EditText = activity.findViewById<EditText>(R.id.mail_address_form)
         mailAddressFormEt.setText("test@mail.com")
-        val textLength = mailAddressFormEt.text.length
-        assertTrue(textLength < 20)
+        assertTrue(activity.textLongerThanLimit(mailAddressFormEt, 10))
     }
 
     @Test
     fun birthdaySpinner_equalToBirthdayConfirmText_ExceptionThrown() {
         val confirmText: TextView = activity.findViewById<TextView>(R.id.birthday_confirm_text)
-        val birthdayYearSpinner: Spinner = activity.findViewById<Spinner>(R.id.birthday_year_spinner)
-        val birthdayMonthSpinner: Spinner = activity.findViewById<Spinner>(R.id.birthday_month_spinner)
+        val birthdayYearSpinner: Spinner =
+            activity.findViewById<Spinner>(R.id.birthday_year_spinner)
+        val birthdayMonthSpinner: Spinner =
+            activity.findViewById<Spinner>(R.id.birthday_month_spinner)
         val birthdayDaySpinner: Spinner = activity.findViewById<Spinner>(R.id.birthday_day_spinner)
 
         val spinnerText: String = "2000年01月01日"
@@ -55,15 +55,13 @@ class ExampleUnitTest {
     fun addressFormText_longerThan20characters_ExceptionThrown() {
         val addressFormEt: EditText = activity.findViewById<EditText>(R.id.address_form)
         addressFormEt.setText("test@mail.com")
-        val textLength = addressFormEt.text.length
-        assertTrue(textLength < 20)
+        assertTrue(activity.textLongerThanLimit(addressFormEt, 20))
     }
 
     @Test
     fun memoFormText_longerThan30characters_ExceptionThrown() {
         val memoFormEt: EditText = activity.findViewById<EditText>(R.id.memo_form)
         memoFormEt.setText("test@mail.com")
-        val textLength = memoFormEt.text.length
-        assertTrue(textLength < 20)
+        assertTrue(activity.textLongerThanLimit(memoFormEt, 30))
     }
 }
